346678958e1379f343f9905d58a4687bee333b69
Roboadh <adikarijamuna114@gmail.com>
2024-09-20 11:14:26 +0545

    Add java file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Robo
 This is my first git repository<br>
-Author-Robart Adhikari
+Author-Robart Adhikari(Nepal)
---
+++ naya.java
@@ -0,0 +1,8 @@
+package Robo;
+
+public class naya {
+    public static void main(String[] args) {
+        System.out.println("My name is RObert");
+    }
+
+}
